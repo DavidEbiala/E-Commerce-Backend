2d31fd7a8a9ff033f0f739b1b3e42d862ba01265
Zino Ebiala <eebia002@fiu.edu>
2023-07-06 20:43:01 -0400

    added productall and productone

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,13 +4,62 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
+  try{
+    const productData = await Product.findAll({
+        include: [
+            {
+                model: Category
+            },
+            {
+                model: Tag,
+                attributes: ['tag_name'],
+                through: ProductTag,
+                as: 'productTag_tag'
+            }
+        ]
+      })
+    res.json(productData)
+  }catch(err){
+    console.log(err);
+    res.status(500).json(err);
+  }
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+    try{
+        const productData = Product.findOne({
+            where: {
+                id: req.params.id
+            },
+            include:[
+                {
+                    model: Category
+                },
+                {
+                    model: Tag,
+                    attributes: ['tag_name'],
+                    through: ProductTag,
+                    as: 'productTag_tag'
+                }
+            ]
+        });
+        if(!productData){
+            res.status(404).json(
+                {
+                    message: 'Product was not found'
+                });
+                return;
+        }else{
+            res.json(productData);
+        }
+    }catch(err){
+        console.log(err);
+        res.status(500).json(err);
+    }
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
