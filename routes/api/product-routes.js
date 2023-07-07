64dcb17ea5d0501b053686b60414d56d53e68b74
Zino Ebiala <eebia002@fiu.edu>
2023-07-06 20:49:23 -0400

    done with product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -141,8 +141,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+   const productData= await Product.destroy({
+        where:{
+            id: req.params.id
+        }
+      })
+      if(!productData){
+        res.status(404).json({message: 'Product doesnt exist'});
+        return;
+      }
+      res.json(productData);
+  }catch(err){
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
